f7e354a7bd72aefc9aa2709de65af94b4a4af91e
nikivikk <nikiforenko2110@gmail.com>
2023-10-19 23:40:33 +0300

    change appointing_order method

--- app/main/controller/app_controller.py
+++ app/main/controller/app_controller.py
@@ -63,7 +63,7 @@ def delete_driver(id):
     return delete_driver_service(id)
 
 
-@controller_blueprint.route('/api/v1/appoint-order/<int:id>')
+@controller_blueprint.route('/api/v1/appoint-order/<int:id>', methods=['POST'])
 def appoint_order(id):
     return appoint_order_service(id)
 
